23165c65bbbe0bddb3171568626083c636fcab8c
marcosdieguezc <mdieguez@cifpfbmoll.eu>
2024-02-23 20:15:26 +0100

    vacunas

--- src/js/application.js
+++ src/js/application.js
@@ -10,7 +10,7 @@ var ages  = ['9 Weeks', '12 Weeks', '3 Months',
              '4 Months', '2 Months', '14 Weeks',
              '6 Months', '10 Weeks', '8 Months'];
 
-var vaccine = ['Vacunas: 5', 'Vacuna: 2', 'Vacuna: 6',
+var vaccines = ['Vacunas: 5', 'Vacunas: 2', 'Vacunas: 6',
              'Vacunas: 3', 'Vacunas: 1', 'Vacunas: 6',
              'Vacunas: 7', 'Vacunas: 8', 'Vacunas: 3',
              'Vacunas: 0', 'Vacunas: 1', 'Vacunas: 52']; 
@@ -20,7 +20,7 @@ $('.kittens').find('li').each( function(i, el) {
   var img = $(el).find('img');
   var name = $(el).find('.name');
   var age = $(el).find('.age');
-  var vaccine = $(el).finf('.vaccine');
+  var vaccine = $(el).find('.vaccine'); // Corregido el typo en 'find'
 
   var w = 250;
   var h = 250;
@@ -28,5 +28,5 @@ $('.kittens').find('li').each( function(i, el) {
   img.attr('src', 'http://placekitten.com/'+w+'/'+h+'?image='+i);
   name.text(names[i]);
   age.text(ages[i]);
-  vaccine.text(vaccine[i]);
+  vaccine.text(vaccines[i]); // Corregido el nombre de la variable aquí
 });
